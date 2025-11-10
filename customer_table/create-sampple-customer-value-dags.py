bbfbc607fd42da9f8e9e037e395da02794d61f19
Đàm Quang Thịnh <thinhquangshin@gmail.com>
2025-11-10 17:21:28 +0700

    update

--- customer_table/create-sampple-customer-value-dags.py
+++ customer_table/create-sampple-customer-value-dags.py
@@ -5,13 +5,13 @@ from datetime import datetime
 default_args = {"owner": "airflow", "retries": 0}
 
 # Maven coordinates (Spark sẽ tự tải)
-PACKAGES = ",".join([
-    "io.delta:delta-spark_2.12:3.2.0",
-    "org.apache.hadoop:hadoop-aws:3.3.4",
-    "org.postgresql:postgresql:42.7.3",
-    "org.apache.hive:hive-metastore:3.1.3",
-    "org.apache.hive:hive-exec:3.1.3"
-])
+# PACKAGES = ",".join([
+#     "io.delta:delta-spark_2.12:3.2.0",
+#     "org.apache.hadoop:hadoop-aws:3.3.4",
+#     "org.postgresql:postgresql:42.7.3",
+#     "org.apache.hive:hive-metastore:3.1.3",
+#     "org.apache.hive:hive-exec:3.1.3"
+# ])
 
 with DAG(
     dag_id="spark_create_customer_delta_tables",
@@ -23,15 +23,15 @@ with DAG(
 
     create_delta_job = SparkSubmitOperator(
         task_id="create_customer_demo",
-        application="local:///opt/spark/app/create_customer_data.py",
+        application="s3a://asset/spark-jobs/create_customer_data.py",
         deploy_mode="cluster",
         name="spark-delta-create",
         conn_id="spark_k8s",
         conf={
             "spark.kubernetes.namespace": "compute",
-            "spark.kubernetes.container.image": "ghostwood/spark-delta-samp-job:2.0.2",
+            "spark.kubernetes.container.image": "ghostwood/spark-delta-samp-job:2.0.5",
             "spark.kubernetes.authenticate.driver.serviceAccountName": "spark",
-            "spark.jars.packages": PACKAGES,
+            # "spark.jars.packages": PACKAGES,
             "spark.sql.extensions": "io.delta.sql.DeltaSparkSessionExtension",
             "spark.sql.catalog.spark_catalog": "org.apache.spark.sql.delta.catalog.DeltaCatalog",
             "spark.sql.catalogImplementation": "hive",
